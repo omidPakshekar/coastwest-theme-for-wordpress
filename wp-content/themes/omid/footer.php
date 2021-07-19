63bcd35c6e36da0fb115a96995c344eba02fb47c
omid pak <omid.pak.developer@gmail.com>
2021-07-20 01:45:15 +0430

    create custom page

--- wp-content/themes/omid/footer.php
+++ wp-content/themes/omid/footer.php
@@ -13,19 +13,59 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
-
 <div class="wrapper" id="wrapper-footer">
 
+    <div class="footer-testimonial">
+        <div class="container">
+            <div class="testimonial">
+                <div class="stars">
+                    <i class="fa fa-star"> </i>
+                    <i class="fa fa-star"> </i>
+                    <i class="fa fa-star"> </i>
+                    <i class="fa fa-star"> </i>
+                    <i class="fa fa-star"> </i>
+                </div>
+                <div class="quote">
+                    amazing customer service and very good price will be definetly getting another one through ghese guys when need to much help thankyou
+                </div>
+                <div class="author">
+                    Damon, facebook review
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="container">
+        <div class="footer-contact-info">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="title">Wantto know more?</div>
+                    <div class="desc"> Cody takes Barbara to be repaired and learns they have more in common than she thought.</div>
+                    <div class="contact-info">
+                        <div class="phone"> <i class="fa fa-phone"></i>call today 09382162999</div>
+                        <div class="email"><i class="fa fa-envelope-o"></i>omid.pakshekar.est@gmail.com</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-form-holder">
+                        <?php echo do_shortcode('[contact-form-7 id="27" title="Footer Contact Form"]')  ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-</div><!-- wrapper end -->
+    <div class="copyright">
+        <div class="container">
+            copyright west coast waste 2020 <span>//</span> <a href="#"> Term and condition</a> <span>//</span> <a href="#">website by moshi moshi</a>
+        </div>
+    </div>
 
-</div><!-- #page we need this extra closing tag here -->
+</div>
+
 
 <?php wp_footer(); ?>
 
 </body>
 
 </html>
-
--- wp-content/themes/omid/page.php
+++ wp-content/themes/omid/page.php
@@ -19,34 +19,58 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
+<div class="page-header-holder">
+	<div class="container">
+		<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		</header><!-- .entry-header -->
+	</div>
+</div>
+
 <div class="wrapper" id="page-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<div class="col-lg-7">
+				<main class="site-main" id="main">
 
-			<main class="site-main" id="main">
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page' );
 
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
 					}
-				}
-				?>
+					?>
 
-			</main><!-- #main -->
+				</main><!-- #main -->
+			</div>
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+			<div class="offset-lg-1 col-lg-4">
+				<div class="right-sidebar-promotion">
+				<div class="discount-circle">
+						<div class="abs-holder">
+							<div class="save">Save</div>
+							<div class="percent">20%</div>
+						</div>
+					</div>
+					<div class="entry-content">
+						<div class="title">15 Day Store Special on 9m3 Skip Bins</div>
+						<div class="desc">BIN SERVICES - Perth , Fremantle, Kwinana, Rockingham, Mandurahm, Pinjarra, Waroona, Harbey Australind, Bunbury, Busselton, Margert River</div>
+						<a  herf="#" class="button outline">find information </a>
+					</div>
+				</div>	
+			</div>
+			
 
+	
 		</div><!-- .row -->
 
 	</div><!-- #content -->
